62ea41a05f448f595a5373e9119b7137657cc93a
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-23 21:40:29 -0700

    work on index.js and html-template.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const inquirer = require("inquirer")
+const fs = require('fs')
 const Manager = require("./lib/Manager");
+const renderTeam = require("./src/html-templates")
 
-const teamMemeberObjArr = [];
+const teamMemberObjArr = [];
 
 const init = () => {
     const createManager = () => {
@@ -41,33 +43,74 @@ const init = () => {
 
     function addEmployees() {
         inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What employees would you like to add?',
-      name: 'choice',
-      choices: ['Engineer', 'Intern', "I'm Done"],
-    },
-  ])
-  .then(answer => {
-      switch (answer.choice) {
-          case 'Engineer':
-              createEngineer()
-              break;
-      }
-  })
+            .prompt([
+                {
+                type: 'list',
+                message: 'What employees would you like to add?',
+                name: 'choice',
+                choices: ['Engineer', 'Intern', "I'm Done"],
+                },
+            ])
+            .then(answer => {
+                switch (answer.choice) {
+                    case 'Engineer':
+                        createEngineer();
+                        break;
+
+                    case 'Intern':
+                        createIntern();
+                        break;
+                        
+                    default:
+                        buildTeam();
+                        break;
+                }
+            });
+
+
+    }
+    function createEngineer() {
+        inquirer
+            .prompt([
+                {
+                type: "input",
+                name: "id",
+                message: "What is the Engineer Id?",
+                },
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is the Engineer name?",
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "What is the Engineer email?",
+                },
+                {
+                    type: "input",
+                    name: "github",
+                    message: "What is the Engineers Github?",
+                },
+            ])
+            .then(answers => {
+                const manager = new Engineer(
+                    answers.id,
+                    answers.name,
+                    answers.email,
+                    answers.github
+                )
+                teamMemeberObjArr.push(engineer)
+                addEmployees()
+            }); 
+        
     }
 
+    function buildTeam() {
+        fs.writeFile("./dist/index.html", renderTeam(teamMemberObjArr), "utf-8")
+    }
 
-
-
-
-
-
-
-
-    createManager()
+    createManager();
 };
-    }
-
-init()
+                
+init();
