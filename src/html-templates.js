eb56fd636000b1b3bfa35fb2f8e75e05d353267a
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-26 01:51:21 -0700

    fixed link and style

--- src/html-templates.js
+++ src/html-templates.js
@@ -3,11 +3,13 @@ const makeTeam = team => {
     const generateManagerCard = manager => {
         return `
         
-    <div class = "col-lg4-md2 gap-3 d-flex justify-content-center">
+    <div class = "col-lg-4 gap-3 d-flex justify-content-center">
         <div class="card" style="width: 18rem; height: 16rem;" id="card">
         <div class="card-body">
           <h5 class="card-title">${manager.name}</h5>
-          <h5 class="card-title">Manager</h5>
+          <h5 class="card-title"><span class="material-symbols-outlined">
+          local_cafe
+          </span>Manager</h5>
         </div>
         <ul class="list-group list-group-flush"> 
         <li class="list-group-item">ID:${manager.id}</li>
@@ -23,16 +25,18 @@ const makeTeam = team => {
 
     const generateEngineerCard = engineer => {
         return `
-    <div class = "col-lg4-md2 gap-3 d-flex justify-content-center">
+    <div class = "col-lg-4 gap-3 d-flex justify-content-center">
         <div class="card" style="width: 18rem; height: 16rem;" id="card">
         <div class="card-body">
           <h5 class="card-title">${engineer.name}</h5>
-          <h5 class="card-title">Engineer</h5>
+          <h5 class="card-title"><span class="material-symbols-outlined">
+          keyboard
+          </span>Engineer</h5>
         </div>
         <ul class="list-group list-group-flush"> 
         <li class="list-group-item">ID:${engineer.id}</li>
         <a href="mailto:${engineer.email}"><li class="list-group-item">email:${engineer.email}</li></a>
-        <li class="list-group-item">Github:${engineer.github}</li>
+        <li class="list-group-item">GitHub: <a href = "https://github.com/${engineer.github}" target = "_blank">${engineer.github}</a></li>
         </ul>
         </div>
     </div>
@@ -44,11 +48,13 @@ const makeTeam = team => {
 
     const generateInternCard = intern => {
         return `
-    <div class = "col-lg4-m2 gap-3 d-flex justify-content-center">
+    <div class = "col-lg-4 gap-3 d-flex justify-content-center">
         <div class="card" style="width: 18rem; height: 16rem;" id="card">
         <div class="card-body">
           <h5 class="card-title">${intern.name}</h5>
-          <h5 class="card-title">Intern</h5>
+          <h5 class="card-title"><span class="material-symbols-outlined">
+            school
+            </span>Intern</h5>
         </div>
         <ul class="list-group list-group-flush"> 
         <li class="list-group-item">ID:${intern.id}</li>
@@ -92,13 +98,18 @@ module.exports = team => {
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./assets/style.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <title>Team Profile Generator</title>
     </head>
     <body>
     <header>
     <p class="text-center text-white" style="height: 180px; font-size: 60px;background-color: #dc3545;" >My Team</p>
     </header>
+    <div class = "container" style = "width: 800px; height: 1000px;">
+      <div class = "row d-flex justify-content-center flex-wrap ">
     ${makeTeam(team)}
+    </div>
+    </div>
     </body>
 </html>
     `
